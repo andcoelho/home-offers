5bf593bb7de739a0c323c3dfc9c00b422c09d753
Anderson <andcoelho97@gmail.com>
2021-10-29 15:20:32 -0300

    .

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ const prencherFormulario = (endereco) => {
 
 const pesquisarCep = async() => { // função assíncrona
     const cep = document.getElementById("cep").value;
-    const url = `http://viacep.com.br/ws/${cep}/json/`;
+    const url = `https://viacep.com.br/ws/${cep}/json/`;
     //fetch(url).then(responde => responde.json()).then(console.log); //Retorna valor da URL
     const dados = await fetch(url); //await - aguardar a promessa de fetch
     const endereco = await dados.json(); //await pois json tbm é promessa
